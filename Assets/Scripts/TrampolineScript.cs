4b20f8ce7e0c46b2fe8ec3236b1431cbc7fd2a30
Starchy29 <ethanball29@gmail.com>
2023-02-02 15:57:58 -0500

    trampolines work, but they can be clipped through sometimes

--- Assets/Scripts/TrampolineScript.cs
+++ Assets/Scripts/TrampolineScript.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class TrampolineScript : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            // make sure the object made contact from the top or bottom
             float xPos = collision.gameObject.transform.position.x;
             if(xPos <= transform.position.x - transform.localScale.x / 2 || xPos >= transform.position.x + transform.localScale.x / 2)
             {
-                return;
+                // horizontal bounce
+                rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            } else {
+                // vertical bounce
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
             }
-
-            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
         }
     }
 }
