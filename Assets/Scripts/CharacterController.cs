807fb2a7b07eee9c7f5319d272e47cac85041341
Adhiraj.B <42261295+ReanSchwarzer1@users.noreply.github.com>
2023-02-02 00:00:02 -0500

    added a simple main menu bg

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -29,34 +29,6 @@ public class CharacterController: MonoBehaviour
         CameraFollow();
     }
 
-    private void CameraFollow()
-    {
-        _cameraTransform.position = Vector3.Lerp(
-            _cameraTransform.position,
-            new Vector3(transform.position.x, transform.position.y,
-            _cameraTransform.position.z), Time.deltaTime * _cameraSpeed);
-
-        _cameraTransform.rotation = Quaternion.identity;
-    }
-
-    private void ShootEgg()
-    {
-        //Shootegg function calls all the functions required to shoot the egg towards the screenspace the player clicks in
-        var direction = GetProjectileDirection();
-        var projectile = ProjectileEgg(direction);
-        ApplyJumpForceOppToShootDir(projectile, direction);
-
-        //code to flip the chicken towards the side they are jumping in / flip the chicken opposite the side they are throwing the eggs
-        if (direction.x > 0)
-        {
-            _chickenRenderer.flipX = true;
-        }
-        else
-        {
-            _chickenRenderer.flipX = false;
-        }
-    }
-
     private Vector2 GetProjectileDirection()
     {
         //to get the transform/position where the player is clickin in
@@ -67,14 +39,42 @@ public class CharacterController: MonoBehaviour
         return (Vector2)(mousePos - transform.position).normalized;
     }
 
-    private GameObject ProjectileEgg(Vector2 direction)
-    {
-        return Instantiate(_eggPrefab, (Vector2)transform.position + direction, Quaternion.identity); //intanciating the eggs towards the direction the player is clicking in
-    }
 
     private void ApplyJumpForceOppToShootDir(GameObject projectile, Vector2 direction)
     {
         projectile.GetComponent<Rigidbody2D>().velocity = direction * _jumpForce; //applying force to the rigidbody to make the player jump
-        _rb.velocity = -direction * _jumpForce; //the rigibody will jump in the direct opposite direction from where the (mousepos var / direction the player clicks in) is
+        _rb.velocity = (_jumpForce * -direction); //the rigibody will jump in the direct opposite direction from where the (mousepos var / direction the player clicks in) is
     }
+
+    private void ShootEgg()
+    {
+        //Shootegg function calls all the functions required to shoot the egg towards the screenspace the player clicks in
+        var direction = GetProjectileDirection();
+        var projectile = Instantiate(_eggPrefab, (Vector2)transform.position + direction, Quaternion.identity); //intanciating the eggs towards the direction the player is clicking in
+        ApplyJumpForceOppToShootDir(projectile, direction);
+
+        FlipChicken(direction);
+    }
+    private void FlipChicken(Vector2 direction)
+    {
+        //code to flip the chicken towards the side they are jumping in / flip the chicken opposite the side they are throwing the eggs
+        if (direction.x > 0)
+        {
+            _chickenRenderer.flipX = false;
+        }
+        else
+        {
+            _chickenRenderer.flipX = true;
+        }
+    }
+    private void CameraFollow()
+    {
+        _cameraTransform.position = Vector3.Lerp(
+            _cameraTransform.position,
+            new Vector3(transform.position.x, transform.position.y,
+            _cameraTransform.position.z), Time.deltaTime * _cameraSpeed);
+
+        _cameraTransform.rotation = Quaternion.identity;
+    }
+
 }
