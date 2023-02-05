a8118f12f38e979f8df2f9db7d49520ca3ebab29
sj3603 <56056374+sj3603@users.noreply.github.com>
2023-02-05 14:09:15 -0500

    Begun work on plant enemy
    - Added area trigger to the plant
    - changed the filtering on the plant texture so its not shit
    - updated plant script so plant can be made to face left or right

--- Assets/Scripts/PlantScript.cs
+++ Assets/Scripts/PlantScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlantScript : MonoBehaviour
 {
     [SerializeField] Animator animatorScript;
+    [SerializeField] BoxCollider2D areaTrigger;
     [SerializeField] GameObject peaObject;
-    [SerializeField] bool isFacingLeft;
-    [SerializeField] bool isShooting;
+    [SerializeField] bool isFacingRight;
+    GameObject playerObject;
+    private bool isShooting;
 
     // Start is called before the first frame update
     void Start()
@@ -15,23 +17,50 @@ public class PlantScript : MonoBehaviour
         //Set up the animator in scripts and set the plant to idleing
         animatorScript = GetComponent<Animator>();
         animatorScript.SetBool("idle", true);
-        if (!isFacingLeft)
+        if (isFacingRight)
         {
             //Should flip the plant so it's facing the other way
+            gameObject.transform.localScale = new Vector3(-gameObject.transform.localScale.x, gameObject.transform.localScale.y, 1);
         }
+        playerObject = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerObject.GetComponent<Collider2D>().IsTouching(areaTrigger))
+        {
+            isShooting = true;
+        }
+        else
+        {
+            isShooting = false;
+        }
+
         animatorScript.SetBool("shooting", isShooting);
         animatorScript.SetBool("idle", !isShooting);
     }
 
     void ShootPea()
     {
-        animatorScript.SetBool("shooting", true);
+        Debug.Log("The Pea has been shot");
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.IsTouching(areaTrigger))
+    //    {
+    //        isShooting = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (!collision.IsTouching(areaTrigger))
+    //    {
+    //        isShooting = false;
+    //    }
+    //}
 
     //have a invisible trigger extending out in front of the pea shooter
     //if the chicken is inside the trigger, repeatedly shoot peas
