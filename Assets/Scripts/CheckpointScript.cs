179a02b78d01302abc9412b877f38bbf24a6796c
sj3603 <56056374+sj3603@users.noreply.github.com>
2023-02-14 13:29:47 -0500

    Sprite and animation cleanup
    - Cleaned up more sprites
    - Fixed checkpoint animations
    - Swapped checkpoint to single object prefab

--- Assets/Scripts/CheckpointScript.cs
+++ Assets/Scripts/CheckpointScript.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class CheckpointScript : MonoBehaviour
 {
+    [SerializeField] Animator animatorScript;
+
+    private void Start()
+    {
+        animatorScript = GetComponent<Animator>();
+        animatorScript.SetBool("Idle", true);
+        animatorScript.SetBool("CheckFlagOut", false);
+        animatorScript.SetBool("CheckFlagIdle", false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         CharacterController playerScript = collision.gameObject.GetComponent<CharacterController>();
         if (playerScript != null)
         {
             GameObject.Find("Checkpoint Tracker").GetComponent<CheckpointTracker>().SpawnPoint = gameObject.transform.position;
-        }
+            animatorScript.SetBool("Idle", false);
+            animatorScript.SetBool("CheckFlagOut", true);
+        }  
+    }
+
+    void SetFlagOutIdle()
+    {
+        Debug.Log("TheFlagIsIdle");
+        animatorScript.SetBool("CheckFlagOut", false);
+        animatorScript.SetBool("CheckFlagIdle", true);
     }
 }
